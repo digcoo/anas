0c5c3ba7336cfbf922223a2ca3e43cbf4ce2d7b0
digcoo <junifer@192.168.1.101>
2018-02-06 15:10:49 +0800

    listForShop bugfix..

--- slife-api/src/main/java/com/slife/utils/SlifeRedisTemplate.java
+++ slife-api/src/main/java/com/slife/utils/SlifeRedisTemplate.java
@@ -1,5 +1,6 @@
 package com.slife.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
@@ -8,6 +9,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+
 import java.util.Set;
 
 @Component
@@ -42,7 +44,8 @@ public class SlifeRedisTemplate {
     }
     
     public int getFavorNum(Long adId){
-    	return Integer.parseInt(stringRedisTemplate.opsForValue().get(RedisKey.ADS_FAVOR_NUM+adId+":favornum"));
+    	String favorNum = stringRedisTemplate.opsForValue().get(RedisKey.ADS_FAVOR_NUM+adId+":favornum");
+		return StringUtils.isEmpty(favorNum)? 0 : Integer.parseInt(favorNum);
     }
 
     /**
