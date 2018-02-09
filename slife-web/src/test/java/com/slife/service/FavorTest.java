6461d43751f965536a4c5be2de14ccecc0ed79c4
digcoo <junifer@192.168.1.103>
2018-02-09 13:41:29 +0800

    commit..

--- slife-web/src/test/java/com/slife/service/FavorTest.java
+++ slife-web/src/test/java/com/slife/service/FavorTest.java
@@ -1,46 +1,46 @@
-package com.slife.service;
-
-import com.alibaba.fastjson.JSON;
-import com.slife.WebApplicationTests;
-import com.slife.api.controller.FavorController;
-import com.slife.api.controller.FollowController;
-import com.slife.base.entity.ReturnDTO;
-import com.slife.entity.ShopAd;
-import org.junit.Test;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-/**
- * Created by cq on 18-1-28.
- */
-public class FavorTest extends WebApplicationTests {
-
-    @Resource
-    private FavorController favorController;
-
-
-    @Test
-    public void link() {
-        long userId = 1;
-        long shopAddId = 1;
-        ReturnDTO<Boolean> returnDTO =  favorController.link(userId ,shopAddId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-    @Test
-    public void unlink() {
-        long userId = 1;
-        long shopAddId = 1;
-        ReturnDTO<Boolean> returnDTO =  favorController.unlink(userId ,shopAddId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-    @Test
-    public void getShopListByUserId() {
-        long userId = 1;
-        ReturnDTO<List<ShopAd>> returnDTO =  favorController.shopAdList(userId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-}
+//package com.slife.service;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.slife.WebApplicationTests;
+//import com.slife.api.controller.FavorController;
+//import com.slife.api.controller.FollowController;
+//import com.slife.base.entity.ReturnDTO;
+//import com.slife.entity.ShopAd;
+//import org.junit.Test;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//
+///**
+// * Created by cq on 18-1-28.
+// */
+//public class FavorTest extends WebApplicationTests {
+//
+//    @Resource
+//    private FavorController favorController;
+//
+//
+//    @Test
+//    public void link() {
+//        long userId = 1;
+//        long shopAddId = 1;
+//        ReturnDTO<Boolean> returnDTO =  favorController.link(userId ,shopAddId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//    @Test
+//    public void unlink() {
+//        long userId = 1;
+//        long shopAddId = 1;
+//        ReturnDTO<Boolean> returnDTO =  favorController.unlink(userId ,shopAddId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//    @Test
+//    public void getShopListByUserId() {
+//        long userId = 1;
+//        ReturnDTO<List<ShopAd>> returnDTO =  favorController.shopAdList(userId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//}
--- slife-web/src/test/java/com/slife/service/FollowTest.java
+++ slife-web/src/test/java/com/slife/service/FollowTest.java
@@ -1,45 +1,45 @@
-package com.slife.service;
-
-import com.alibaba.fastjson.JSON;
-import com.slife.WebApplicationTests;
-import com.slife.api.controller.FollowController;
-import com.slife.base.entity.ReturnDTO;
-import com.slife.entity.Shop;
-import org.junit.Test;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-/**
- * Created by cq on 18-1-28.
- */
-public class FollowTest extends WebApplicationTests {
-
-    @Resource
-    private FollowController followController;
-
-
-    @Test
-    public void link() {
-        long userId = 1;
-        long shopId = 25;
-        ReturnDTO<Boolean> returnDTO =  followController.link(userId ,shopId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-    @Test
-    public void unlink() {
-        long userId = 1;
-        long shopId = 25;
-        ReturnDTO<Boolean> returnDTO =  followController.unlink(userId ,shopId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-    @Test
-    public void getShopListByUserId() {
-        long userId  = 1;
-        ReturnDTO<List<Shop>> returnDTO =  followController.shopList(userId);
-        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
-    }
-
-}
+//package com.slife.service;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.slife.WebApplicationTests;
+//import com.slife.api.controller.FollowController;
+//import com.slife.base.entity.ReturnDTO;
+//import com.slife.entity.Shop;
+//import org.junit.Test;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//
+///**
+// * Created by cq on 18-1-28.
+// */
+//public class FollowTest extends WebApplicationTests {
+//
+//    @Resource
+//    private FollowController followController;
+//
+//
+//    @Test
+//    public void link() {
+//        long userId = 1;
+//        long shopId = 25;
+//        ReturnDTO<Boolean> returnDTO =  followController.link(userId ,shopId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//    @Test
+//    public void unlink() {
+//        long userId = 1;
+//        long shopId = 25;
+//        ReturnDTO<Boolean> returnDTO =  followController.unlink(userId ,shopId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//    @Test
+//    public void getShopListByUserId() {
+//        long userId  = 1;
+//        ReturnDTO<List<Shop>> returnDTO =  followController.shopList(userId);
+//        System.out.println("link result:"+ JSON.toJSONString(returnDTO));
+//    }
+//
+//}
